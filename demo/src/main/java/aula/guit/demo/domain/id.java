1544da0451631cc86042a98f9cd7b0e1382ff27b
jacksobjunior12 <jacksobjunior12@gmial.com>
2023-03-16 19:31:32 -0300

    aula git2

---
+++ demo/src/main/java/aula/guit/demo/domain/id.java
@@ -0,0 +1,14 @@
+package aula.guit.demo.domain;
+
+public class id {
+    Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
---
+++ demo/src/main/java/aula/guit/demo/domain/nome.java
@@ -0,0 +1,32 @@
+package aula.guit.demo.domain;
+
+public class nome {
+    String nome;
+    String idade;
+    String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+}
